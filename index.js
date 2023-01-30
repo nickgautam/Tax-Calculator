3756600d90a14b7cc036d41f6b227b71e696cf66
nickgautam <er.nishantgautam0011@gmail.com>
2023-01-30 22:52:53 +0530

    modified

--- index.js
+++ index.js
@@ -17,16 +17,17 @@ let employeeContribution = 0;
 let totalIncome = 0;
 let totalDeduction = 0;
 let taxableIncome = 0;
-let tax = 0;
+let oldRegimeTax = 0;
+let newRegimeTax = 0;
 let regime = null;
 let ageData = null;
-
-
+let income = 0;
+let selectRegime=null
+let chooseRegime=null
 
 //--------------------------------------- Radio button function to get values -----------------------------------------------//
 
 function displayRadioValue() {
-
   var ele = document.getElementsByName("year");
   var age = document.getElementsByName("age");
 
@@ -37,11 +38,9 @@ function displayRadioValue() {
   for (i = 0; i < age.length; i++) {
     if (age[i].checked) ageData = age[i].value;
   }
-
 }
 
 displayRadioValue();
-
 
 //-----------------------------------Functions to get the value of required fields-------------------------------------------//
 
@@ -101,77 +100,74 @@ function getemployeeContribution(el) {
   employeeContribution = +el.value;
 }
 
-
-
 //---------------------------------------------Tax Calculation function-------------------------------------------------------//
 
 function calculate() {
+  if (ageData == "0 to 60") {
 
-  if (regime == "old-regime" && ageData == "0 to 60") {
+    income = salary + interest + otherIncome + homeLoan + rentalIncome + interestLoan;
 
-    totalIncome = salary + interest + otherIncome + homeLoan + rentalIncome + interestLoan - deduction;
-    totalDeduction = basicDeduction + interestDeposit + medicalInsurance + charityDonation + educationalLoan + housingLoan + employeeContribution;
+    totalIncome = income - deduction;
+
+    totalDeduction =basicDeduction +interestDeposit +medicalInsurance +charityDonation +educationalLoan +housingLoan +employeeContribution;
 
     document.getElementById("old_gross").innerHTML = totalIncome;
     document.getElementById("old_deduction").innerHTML = totalDeduction;
-    document.getElementById("new_gross").innerHTML = 0
+    document.getElementById("new_gross").innerHTML = income;
     document.getElementById("new_deduction").innerHTML = 0;
-
+   
     taxableIncome = totalIncome - totalDeduction;
+    // console.log(taxableIncome)
 
     if (taxableIncome == 0) {
       document.getElementById("old_tax").innerHTML = 0;
       document.getElementById("new_tax").innerHTML = 0;
     }
-
     else {
-
       if (taxableIncome <= 250000) {
-        tax = 0;
-        tax = tax + (tax * 4) / 100;
+        oldRegimeTax = 0;
+        oldRegimeTax = oldRegimeTax + (oldRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = tax;
-        document.getElementById("new_tax").innerHTML = 0;
+        document.getElementById("old_tax").innerHTML = oldRegimeTax;
+       
       }
 
-      if (taxableIncome > 250000 && taxableIncome <= 500000) {
-        tax = ((taxableIncome - 250000) * 5) / 100;
-        tax = tax + (tax * 4) / 100;
+      else if (taxableIncome > 250000 && taxableIncome <= 500000) {
+        oldRegimeTax = ((taxableIncome - 250000) * 5) / 100;
+        oldRegimeTax = oldRegimeTax + (oldRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = tax;
-        document.getElementById("new_tax").innerHTML = 0;
+        document.getElementById("old_tax").innerHTML = oldRegimeTax;
+       
       }
 
-      if (taxableIncome > 500000 && taxableIncome <= 1000000) {
-        tax = ((taxableIncome - 500000) * 20) / 100 + 12500;
-        tax = tax + (tax * 4) / 100;
+      else if (taxableIncome > 500000 && taxableIncome <= 1000000) {
+        oldRegimeTax = ((taxableIncome - 500000) * 20) / 100 + 12500;
+        oldRegimeTax = oldRegimeTax + (oldRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = tax;
-        document.getElementById("new_tax").innerHTML = 0;
+        document.getElementById("old_tax").innerHTML = oldRegimeTax;
+       
       }
 
-      if (taxableIncome > 1000000) {
-        tax = ((taxableIncome - 1000000) * 30) / 100 + 112500;
-        tax = tax + (tax * 4) / 100;
+      else if (taxableIncome > 1000000) {
+        oldRegimeTax = ((taxableIncome - 1000000) * 30) / 100 + 112500;
+        oldRegimeTax = oldRegimeTax + (oldRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = tax;
-        document.getElementById("new_tax").innerHTML = 0;
+        document.getElementById("old_tax").innerHTML = oldRegimeTax;
+     
       }
-
     }
-
   }
 
-  if (regime == "old-regime" && ageData == "60 to 80") {
-
-    totalIncome = salary + interest + otherIncome + homeLoan + rentalIncome + interestLoan - deduction;
+  if (ageData == "60 to 80") {
+    income = salary + interest + otherIncome + homeLoan + rentalIncome + interestLoan;
+    totalIncome = income - deduction;
     totalDeduction = basicDeduction + interestDeposit + medicalInsurance + charityDonation + educationalLoan + housingLoan + employeeContribution;
 
     document.getElementById("old_gross").innerHTML = totalIncome;
     document.getElementById("old_deduction").innerHTML = totalDeduction;
-    document.getElementById("new_gross").innerHTML = 0
+    document.getElementById("new_gross").innerHTML = income;
     document.getElementById("new_deduction").innerHTML = 0;
-
+   
     taxableIncome = totalIncome - totalDeduction;
 
     if (taxableIncome == 0) {
@@ -180,53 +176,47 @@ function calculate() {
     }
 
     else {
-
       if (taxableIncome <= 300000) {
-        tax = 0;
-        tax = tax + (tax * 4) / 100;
+        oldRegimeTax = 0;
+        oldRegimeTax = oldRegimeTax + (oldRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = tax;
-        document.getElementById("new_tax").innerHTML = 0;
+        document.getElementById("old_tax").innerHTML = oldRegimeTax;
       }
-
 
       if (taxableIncome > 300000 && taxableIncome <= 500000) {
-        tax = ((taxableIncome - 300000) * 5) / 100;
-        tax = tax + (tax * 4) / 100;
+        oldRegimeTax = ((taxableIncome - 300000) * 5) / 100;
+        oldRegimeTax = oldRegimeTax + (oldRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = tax;
-        document.getElementById("new_tax").innerHTML = 0;
+        document.getElementById("old_tax").innerHTML = oldRegimeTax;
       }
 
       if (taxableIncome > 500000 && taxableIncome <= 1000000) {
-        tax = ((taxableIncome - 500000) * 20) / 100 + 10000;
-        tax = tax + (tax * 4) / 100;
+        oldRegimeTax = ((taxableIncome - 500000) * 20) / 100 + 10000;
+        oldRegimeTax = oldRegimeTax + (oldRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = tax;
-        document.getElementById("new_tax").innerHTML = 0;
+        document.getElementById("old_tax").innerHTML = oldRegimeTax;
       }
 
       if (taxableIncome > 1000000) {
-        tax = ((taxableIncome - 1000000) * 30) / 100 + 110000;
-        tax = tax + (tax * 4) / 100;
+        oldRegimeTax = ((taxableIncome - 1000000) * 30) / 100 + 110000;
+        oldRegimeTax = oldRegimeTax + (oldRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = tax;
-        document.getElementById("new_tax").innerHTML = 0;
+        document.getElementById("old_tax").innerHTML = oldRegimeTax;
       }
-
     }
-
   }
 
-  if (regime == "old-regime" && ageData == "80 & above") {
-    totalIncome = salary + interest + otherIncome + homeLoan + rentalIncome + interestLoan - deduction;
-    totalDeduction = basicDeduction + interestDeposit + medicalInsurance + charityDonation + educationalLoan + housingLoan + employeeContribution;
+  if (ageData == "80 & above") {
+
+    income =salary + interest + otherIncome + homeLoan + rentalIncome + interestLoan;
+    totalIncome = income - deduction;
+    totalDeduction =basicDeduction +interestDeposit +medicalInsurance +charityDonation +educationalLoan +housingLoan +employeeContribution;
 
     document.getElementById("old_gross").innerHTML = totalIncome;
     document.getElementById("old_deduction").innerHTML = totalDeduction;
-    document.getElementById("new_gross").innerHTML = 0
+    document.getElementById("new_gross").innerHTML = income;
     document.getElementById("new_deduction").innerHTML = 0;
-
+   
     taxableIncome = totalIncome - totalDeduction;
 
     if (taxableIncome == 0) {
@@ -235,113 +225,113 @@ function calculate() {
     }
 
     else {
-
       if (taxableIncome <= 500000) {
-        tax = 0
-        tax = tax + (tax * 4) / 100;
+        oldRegimeTax = 0;
+        oldRegimeTax = oldRegimeTax + (oldRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = tax;
-        document.getElementById("new_tax").innerHTML = 0;
+        document.getElementById("old_tax").innerHTML = oldRegimeTax;
       }
 
-      if (taxableIncome > 500000 && taxableIncome <= 1000000) {
-        tax = ((taxableIncome - 500000) * 20) / 100;
-        tax = tax + (tax * 4) / 100;
+      else if (taxableIncome > 500000 && taxableIncome <= 1000000) {
+        oldRegimeTax = ((taxableIncome - 500000) * 20) / 100;
+        oldRegimeTax = oldRegimeTax + (oldRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = tax;
-        document.getElementById("new_tax").innerHTML = 0;
+        document.getElementById("old_tax").innerHTML = oldRegimeTax;
       }
 
-      if (taxableIncome > 1000000) {
-        tax = ((taxableIncome - 1000000) * 30) / 100 + 100000;
-        tax = tax + (tax * 4) / 100;
+      else if (taxableIncome > 1000000) {
+        oldRegimeTax = ((taxableIncome - 1000000) * 30) / 100 + 100000;
+        oldRegimeTax = oldRegimeTax + (oldRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = tax;
-        document.getElementById("new_tax").innerHTML = 0;
+        document.getElementById("old_tax").innerHTML = oldRegimeTax;
       }
-
     }
-
   }
 
-  if (regime == "new-regime" && (ageData == "0 to 60" || "60 to 80" || "80 & above")) {
+  if ((ageData == "0 to 60" || "60 to 80" || "80 & above")) {
 
-    totalIncome = salary + interest + otherIncome + homeLoan + rentalIncome + interestLoan;
+    income =salary + interest + otherIncome + homeLoan + rentalIncome + interestLoan;
 
-    document.getElementById("old_gross").innerHTML = 0
-    document.getElementById("old_deduction").innerHTML = 0
-    document.getElementById("new_gross").innerHTML = totalIncome;
+    totalIncome = income - deduction;
+
+    totalDeduction =basicDeduction +interestDeposit +medicalInsurance +charityDonation +educationalLoan +housingLoan +employeeContribution;
+
+    document.getElementById("old_gross").innerHTML = totalIncome;
+    document.getElementById("old_deduction").innerHTML = totalDeduction;
+    document.getElementById("new_gross").innerHTML = income;
     document.getElementById("new_deduction").innerHTML = 0;
 
-    taxableIncome = totalIncome;
+    taxableIncome = income;
 
     if (taxableIncome == 0) {
       document.getElementById("old_tax").innerHTML = 0;
       document.getElementById("new_tax").innerHTML = 0;
     }
-
+   
     else {
 
       if (taxableIncome <= 250000) {
-        tax = 0;
-        tax = tax + (tax * 4) / 100;
+        newRegimeTax = 0;
+        newRegimeTax = newRegimeTax + (newRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = 0;
-        document.getElementById("new_tax").innerHTML = tax;
+
+      }
+      else if (taxableIncome > 250000 && taxableIncome <= 500000) {
+        newRegimeTax = ((taxableIncome - 250000) * 5) / 100;
+        newRegimeTax = newRegimeTax + (newRegimeTax * 4) / 100;
+        // console.log(tax)
+       
       }
 
-      if (taxableIncome > 250000 && taxableIncome <= 500000) {
-        tax = ((taxableIncome - 250000) * 5) / 100;
-        tax = tax + (tax * 4) / 100;
+      else if (taxableIncome > 500000 && taxableIncome <= 750000) {
+        newRegimeTax = ((taxableIncome - 500000) * 10) / 100 + 12500;
+        newRegimeTax = newRegimeTax + (newRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = 0;
-        document.getElementById("new_tax").innerHTML = tax;
+     
       }
 
-      if (taxableIncome > 500000 && taxableIncome <= 750000) {
-        tax = ((taxableIncome - 500000) * 10) / 100 + 12500;
-        tax = tax + (tax * 4) / 100;
+      else if (taxableIncome > 750000 && taxableIncome <= 1000000) {
+        newRegimeTax = ((taxableIncome - 750000) * 15) / 100 + 37500;
+        newRegimeTax = newRegimeTax + (newRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = 0;
-        document.getElementById("new_tax").innerHTML = tax;
+     
       }
 
-      if (taxableIncome > 750000 && taxableIncome <= 1000000) {
-        tax = ((taxableIncome - 750000) * 15) / 100 + 37500;
-        tax = tax + (tax * 4) / 100;
+      else if (taxableIncome > 1000000 && taxableIncome <= 1250000) {
+        newRegimeTax = ((taxableIncome - 1000000) * 20) / 100 + 75000;
+        newRegimeTax = newRegimeTax + (newRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = 0;
-        document.getElementById("new_tax").innerHTML = tax;
+   
       }
 
-      if (taxableIncome > 1000000 && taxableIncome <= 1250000) {
-        tax = ((taxableIncome - 1000000) * 20) / 100 + 75000;
-        tax = tax + (tax * 4) / 100;
+      else if (taxableIncome > 1250000 && taxableIncome <= 1500000) {
+        newRegimeTax = ((taxableIncome - 1250000) * 25) / 100 + 125000;
+        newRegimeTax = newRegimeTax + (newRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = 0;
-        document.getElementById("new_tax").innerHTML = tax;
+     
       }
 
-      if (taxableIncome > 1250000 && taxableIncome <= 1500000) {
-        tax = ((taxableIncome - 1250000) * 25) / 100 + 125000;
-        tax = tax + (tax * 4) / 100;
+      else if (taxableIncome > 1500000) {
+        newRegimeTax = ((taxableIncome - 1500000) * 30) / 100 + 187500;
+        newRegimeTax = newRegimeTax + (newRegimeTax * 4) / 100;
         // console.log(tax)
-        document.getElementById("old_tax").innerHTML = 0;
-        document.getElementById("new_tax").innerHTML = tax;             //+ 26520;
+       
       }
 
-      if (taxableIncome > 1500000) {
-        tax = ((taxableIncome - 1500000) * 30) / 100 + 187500;
-        tax = tax + (tax * 4) / 100;
-        // console.log(tax)
-        document.getElementById("old_tax").innerHTML = 0;
-        document.getElementById("new_tax").innerHTML = tax;
+      if(newRegimeTax>oldRegimeTax){
+        selectRegime= "Old Regime";
+        chooseRegime= "New Regime"
+      }
+      else{
+        selectRegime= "New Regime";
+        chooseRegime= "Old Regime"
       }
 
+        document.getElementById("suggestion").innerHTML= `You should opt for ${selectRegime} as it will provide you benefit of Rs. ${Math.abs(newRegimeTax-oldRegimeTax)} over ${chooseRegime}.`
+        document.getElementById("new_tax").innerHTML = newRegimeTax;
+     
     }
-
   }
-
 }
 
 var deductionLimits = {
