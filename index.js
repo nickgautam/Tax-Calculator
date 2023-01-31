594f8a940fd5b20e5523d3f3ea959779fe146bb0
nickgautam <er.nishantgautam0011@gmail.com>
2023-02-01 03:40:45 +0530

    updated 31/01/2023

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+
+function openPage(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  console.log(tabcontent)
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
 //---------------------------------------------- All variables declaration ----------------------------------------------------//
 
 let salary = 0;
